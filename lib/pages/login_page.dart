c91f526b1faed1f3387cd267b5cd94cccf836c48
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-08-23 14:59:04 +0500

    Initial commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(
+        children: [
+          Icon(Icons.message,
+          size: 80,)
+
+
+         // logo
+
+          //Welcome back message
+
+          // email textfield
+
+          // passaword textfield
+
+          // signin button
+
+          // not  a member? register now
+
+
+
+
+
+
+        ],
+
+
+
+      ),
+
+
+
+    );
+  }
+}
