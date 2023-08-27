6f6472d6b9fb024930b3784a1c4e36dd90c783d8
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-08-27 15:27:40 +0500

    adding designs& data time

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatBubble extends StatelessWidget {
   final String message;
-  const ChatBubble({super.key, required this.message});
+ final DateTime dateTime;
+
+  const ChatBubble({super.key, required this.message,required this.dateTime});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +15,39 @@ class ChatBubble extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         color: Colors.blue,
       ),
-      child: Text(message,style: TextStyle(fontSize: 16,color: Colors.white),),
+      child: Column(
+        children: [
+          Text(message,style: TextStyle(fontSize: 16,color: Colors.white),),
+          Text(dateTime.toString()),
+
+
+        ],
+
+
+      )
     );
   }
 }
+
+
+
+class ChatEmailBubble extends StatelessWidget {
+  final String email;
+  const ChatEmailBubble({super.key,required this.email});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 39,
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.grey.shade300,
+      ),
+      child: Text("Email : "+email,style: TextStyle(fontSize: 10,color: Colors.black),),
+    );
+  }
+}
+
+
+
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -37,158 +37,130 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(
         title: Text(widget.receiverUserEmail),
       ),
-
       body: Column(
         children: [
-
           Expanded(child: _buildMessageList()),
 
-
-                //user input
+          //user input
           _buildMessageInput(),
-
-
-
         ],
-
-
-
       ),
     );
   }
 
-
   //buildding message list
 
-  Widget _buildMessageList()
-  {
-
+  Widget _buildMessageList() {
     return StreamBuilder(
-      stream: _chatService.getMessages(widget.receiverUserId, _firebaseAuth.currentUser!.uid),
-
+      stream: _chatService.getMessages(
+          widget.receiverUserId, _firebaseAuth.currentUser!.uid),
       builder: (context, snapshot) {
-
-        if(snapshot.hasError)
-          {
-            return Text('Error'+snapshot.error.toString());
-
-          }
-        if(snapshot.connectionState==ConnectionState.waiting)
-          {
-
-
-            return Text('Loading');
-          }
-
+        if (snapshot.hasError) {
+          return Text('Error' + snapshot.error.toString());
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text('Loading');
+        }
 
         return ListView(
-
-         children:snapshot.data!.docs.map((document) => _buildMessageItem(document)).toList(),
-
-
+          children: snapshot.data!.docs
+              .map((document) => _buildMessageItem(document))
+              .toList(),
         );
-
-
-    },);
-
-
-
-
+      },
+    );
   }
 
+  //build message item
 
-
-    //build message item
-
-  Widget _buildMessageItem(DocumentSnapshot documentSnapshot)
-
-  {
-    Map<String ,dynamic> data=documentSnapshot.data() as Map<String ,dynamic>;
+  Widget _buildMessageItem(DocumentSnapshot documentSnapshot) {
+    Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
 
     // align the messags of receriver to right and other left
-    var alignment=(data['senderId']==_firebaseAuth.currentUser!.uid)
-    ? Alignment.centerRight:Alignment.centerLeft;
+    var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
+        ? Alignment.centerRight
+        : Alignment.centerLeft;
 
     return Container(
-alignment: alignment,
+      alignment: alignment,
       child: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
-          crossAxisAlignment: (data['senderId']==_firebaseAuth.currentUser!.uid)  ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-          mainAxisAlignment: (data['senderId']==_firebaseAuth.currentUser!.uid)  ?MainAxisAlignment.end : MainAxisAlignment.start,
+          crossAxisAlignment:
+              (data['senderId'] == _firebaseAuth.currentUser!.uid)
+                  ? CrossAxisAlignment.end
+                  : CrossAxisAlignment.start,
+          mainAxisAlignment:
+              (data['senderId'] == _firebaseAuth.currentUser!.uid)
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
           children: [
-
-            Text(data['senderEmail']),
-            ChatBubble(message: data['message']),
-
-          
-
-
+            ChatEmailBubble(email: data['senderEmail']),
+            SizedBox(
+              height: 6,
+            ),
+            ChatBubble(message: data['message'],dateTime: DateTime.now())
           ],
+        ),
+      ),
+    );
+  }
+
+  // build message Input.
+
+  Widget _buildMessageInput() {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              controller: _messageController,
+
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.blue),
+
+                ),
+                fillColor: Colors.grey.shade100,
+                filled: true,
+
+
+                border: OutlineInputBorder(
+
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.black)
+
+
+                )
+
+              ),
+              enabled: true,
+
+            ),
+          ),
+
 
 
 
         ),
-      ),
+        IconButton(
+          onPressed: sendMessage,
+          icon: Icon(Icons.send),
+        ),
 
 
+
+
+
+
+
+
+
+
+
+      ],
     );
-
-
-
-
   }
-
-
-    // build message Input,
-
-    Widget _buildMessageInput()
-    {
-      return Row(
-        children: [
-          
-          Expanded(child: MyTextField(
-
-
-              controller: _messageController, hintText: 'Enter Message',
-              obscureText: false),) ,
-          
-          IconButton(onPressed: sendMessage, icon: Icon(Icons.send))
-
-          
-          
-          
-          
-        ],
-        
-        
-        
-        
-      );
-      
-      
-      
-      
-      
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
