ef1f2fbc7abab767d8153dba6b66b4db6217b578
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-08-27 15:37:32 +0500

    Some last moments work remaining

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ChatBubble extends StatelessWidget {
   final String message;
@@ -48,6 +49,5 @@ class ChatEmailBubble extends StatelessWidget {
     );
   }
 }
-
 
 
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,6 +4,7 @@ import 'package:chat_messaging_app/services/chat/chat_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ChatPage extends StatefulWidget {
   final receiverUserEmail;
@@ -21,6 +22,20 @@ class _ChatPageState extends State<ChatPage> {
   final TextEditingController _messageController = TextEditingController();
   final ChatService _chatService = ChatService();
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   void sendMessage() async {
     if (_messageController.text.isNotEmpty) {
